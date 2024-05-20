194be4aa1af2ade690d747cab50dfd29988b15a2
Preetam Beeravelli <preetambeeravelli@gmail.com>
2024-05-20 15:20:30 -0500

    Initial Commit

---
+++ Chuck Norris Jokes/Chuck_Norris_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chuck_Norris_JokesApp.swift
+//  Chuck Norris Jokes
+//
+//  Created by Preetam Beeravelli on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Chuck_Norris_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
