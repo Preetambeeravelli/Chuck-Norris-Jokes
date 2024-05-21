9d1133812796644746f2d09e62235f183c3c0e15
Preetam Beeravelli <preetambeeravelli@gmail.com>
2024-05-21 12:59:03 -0500

    Add UD Layer and fav Page

--- Chuck Norris Jokes/Views/ContentView.swift
+++ Chuck Norris Jokes/Views/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelected: Tab = .house
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                TabView(selection: $tabSelected) {
+                    HomePage().tag(Tab.house)
+                    FavouritesPage().tag(Tab.heart)
+                }
+                VStack {
+                    TabBarView(currentTab: $tabSelected)
+                }
+                .padding(.top, 2)
+            }
         }
-        .padding()
     }
 }
 
